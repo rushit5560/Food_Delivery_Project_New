e287aabc3012e863a2f05a321cafdf05df620eca
Rushit <90950237+rushit5560@users.noreply.github.com>
2021-11-09 14:06:59 +0530

    Today Order Screen UI

--- food_delivery_driver/lib/common/app_images.dart
+++ food_delivery_driver/lib/common/app_images.dart
@@ -4,5 +4,6 @@ class Images{
   static const ic_drawer_menu = '$root' + 'drawer_menu.png';
   static const ic_barcode_scan = '$root' + 'scanning.png';
   static const ic_scanner = '$root' + 'scanner.png';
+  static const ic_category1 = '$root' + 'category1.png';
 
 }
