bc3955ebf25c31d62b4e39e975322108dd35bff1
Rushit <rushit@omtecweb.com>
2022-03-09 13:07:59 +0530

    User App (Splash to Home Screen Validation Done)

--- food_delivery/lib/controllers/splash_screen_controller/splash_screen_controller.dart
+++ food_delivery/lib/controllers/splash_screen_controller/splash_screen_controller.dart
@@ -1,20 +1,24 @@
 import 'dart:async';
-//import 'package:food_delivery/screens/auth_view/auth_screen.dart';
-//import 'package:food_delivery/views/auth_screen/auth_screen.dart';
-//import 'package:food_delivery/screens/sign_in_view/sign_in_screen.dart';
+import 'package:food_delivery/common/constant/user_details.dart';
 import 'package:food_delivery/screens/auth_screen/auth_screen.dart';
+import 'package:food_delivery/screens/index_screen/index_screen.dart';
 import 'package:get/get.dart';
 
 
 class SplashScreenController extends GetxController{
 
-  goToIndexScreen() {
-    Get.offAll(()=> AuthScreen());
+  goToNextScreen() {
+    bool isLoggedIn = UserDetails.isUserLoggedIn;
+    if(isLoggedIn == true) {
+      Get.offAll(()=> IndexScreen());
+    } else {
+      Get.offAll(()=> AuthScreen());
+    }
   }
 
   @override
   void onInit() {
-    Timer(Duration(seconds: 2), () => goToIndexScreen());
+    Timer(Duration(seconds: 2), () => goToNextScreen());
     super.onInit();
   }
 }
